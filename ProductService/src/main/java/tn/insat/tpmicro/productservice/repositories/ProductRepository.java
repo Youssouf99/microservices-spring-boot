b870442c532ca64002d5e46354de4064043111c4
Youssouf <youssouf.brahimazib@vinci-autoroutes.com>
2024-04-18 15:20:21 +0200

    feat : add product repository

--- ProductService/src/main/java/tn/insat/tpmicro/productservice/repositories/ProductRepository.java
+++ ProductService/src/main/java/tn/insat/tpmicro/productservice/repositories/ProductRepository.java
@@ -1,2 +1,7 @@
-package tn.insat.tpmicro.productservice.repositories;public interface ProductRepository {
+package tn.insat.tpmicro.productservice.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tn.insat.tpmicro.productservice.entites.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
