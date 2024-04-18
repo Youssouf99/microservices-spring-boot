1d356431c4a4058d812b87f2410f7786b4092e51
Youssouf <youssouf.brahimazib@vinci-autoroutes.com>
2024-04-18 15:22:10 +0200

    feat : insert data

--- ProductService/src/main/java/tn/insat/tpmicro/productservice/DummyDataCLR.java
+++ ProductService/src/main/java/tn/insat/tpmicro/productservice/DummyDataCLR.java
@@ -1,2 +1,26 @@
-package tn.insat.tpmicro.productservice;public class DummyDataCLR {
+package tn.insat.tpmicro.productservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import tn.insat.tpmicro.productservice.dtos.ProductDto;
+import tn.insat.tpmicro.productservice.services.ProductService;
+
+import java.util.stream.Stream;
+
+@Component
+public class DummyDataCLR implements CommandLineRunner {
+
+    @Autowired
+    private ProductService productService;
+    @Override
+    public void run(String... args) throws Exception {
+        Stream.of("Pencil", "Book", "Eraser")
+                .forEach(name -> productService.
+                        saveProduct(new ProductDto(name)));
+
+        productService.getProducts().forEach(
+                product -> System.out.println(product.getName())
+        );
+    }
 }
