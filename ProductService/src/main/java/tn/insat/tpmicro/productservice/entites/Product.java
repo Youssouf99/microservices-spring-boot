b2ec7881f2de7f740a9a1be06ec75cc1c47b17cd
Youssouf <youssouf.brahimazib@vinci-autoroutes.com>
2024-04-18 15:20:46 +0200

    feat : add product entity

--- ProductService/src/main/java/tn/insat/tpmicro/productservice/entites/Product.java
+++ ProductService/src/main/java/tn/insat/tpmicro/productservice/entites/Product.java
@@ -1,2 +1,20 @@
-package tn.insat.tpmicro.productservice.entites;public class Product {
+package tn.insat.tpmicro.productservice.entites;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
 }
