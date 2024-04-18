8ac9fec3a32954c80000a68d8d62baa4835d5a0d
Youssouf <youssouf.brahimazib@vinci-autoroutes.com>
2024-04-18 15:21:02 +0200

    feat : add product Dto

--- ProductService/src/main/java/tn/insat/tpmicro/productservice/dtos/ProductDto.java
+++ ProductService/src/main/java/tn/insat/tpmicro/productservice/dtos/ProductDto.java
@@ -1,2 +1,14 @@
-package tn.insat.tpmicro.productservice.dtos;public class ProductDto {
+package tn.insat.tpmicro.productservice.dtos;
+
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDto {
+    private String name;
 }
