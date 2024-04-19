80054dc258acd25f82a6962752cede5455cce181
Youssouf <youssouf.brahimazib@vinci-autoroutes.com>
2024-04-19 15:51:08 +0200

    feat(ConfigService): add config config server

--- ProductService/src/main/java/tn/insat/tpmicro/productservice/controllers/ProductController.java
+++ ProductService/src/main/java/tn/insat/tpmicro/productservice/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package tn.insat.tpmicro.productservice.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,17 +9,26 @@ import tn.insat.tpmicro.productservice.services.ProductService;
 
 import java.util.List;
 
-//@RestController
+@RestController
 //@RequestMapping("/")
 public class ProductController {
-    private final ProductService productService;
+
+   private final ProductService productService;
+
+    @Value("${me}")
+    private String me;
 
     public ProductController(ProductService productService) {
-        this.productService = productService;
+       this.productService = productService;
     }
 
-    @GetMapping
-    public List<ProductDto> getAllProducts(){
+    @GetMapping("/")
+   public List<ProductDto> getAllProducts(){
         return productService.getProductsDto();
-    }
+   }
+
+    @RequestMapping("/messages")
+    public String tellMe(){
+        System.out.println("c'est moi qui ai répondu!");
+        return me; }
 }
