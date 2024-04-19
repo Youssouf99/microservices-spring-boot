61f9d7b8d289428f28cb108f84bca11944b791ed
Youssouf <youssouf.brahimazib@vinci-autoroutes.com>
2024-04-19 11:56:07 +0200

    feat(Repository): add Methode ProductByNameInLike

--- ProductService/src/main/java/tn/insat/tpmicro/productservice/repositories/ProductRepository.java
+++ ProductService/src/main/java/tn/insat/tpmicro/productservice/repositories/ProductRepository.java
@@ -1,7 +1,16 @@
 package tn.insat.tpmicro.productservice.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import tn.insat.tpmicro.productservice.entites.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    @Query("select p from Product p where p.name like :name")
+    Page<Product> productByName(@Param("name") String name, Pageable pageable);
+
+    Page<Product> findByNameIsLike(String name, Pageable pageable);
 }
